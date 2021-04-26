f045985b71c2fb368c566def321c69b58290f7bc
fkmg <2794934017@qq.com>
2021-04-26 21:09:58 +0800

    第一次提交

---
+++ spring-boot-common-01/src/main/java/com/sxt/SpringBootCommon01Application.java
@@ -0,0 +1,13 @@
+package com.sxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommon01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCommon01Application.class, args);
+    }
+
+}
---
+++ spring-boot-common-01/src/main/java/com/sxt/bean/User.java
@@ -0,0 +1,37 @@
+package com.sxt.bean;
+
+public class User {
+
+    /**学生id**/
+    private int userid;
+
+    /**学生姓名**/
+    private String username;
+
+    /**学生年龄**/
+    private int userage;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getUserage() {
+        return userage;
+    }
+
+    public void setUserage(int userage) {
+        this.userage = userage;
+    }
+}
